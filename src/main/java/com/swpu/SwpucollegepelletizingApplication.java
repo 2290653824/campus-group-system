be82177d6fbc1ab98cc3fbf3d5a4ecc50b150661
Liyuxi <1318813286@qq.com>
2021-11-02 17:58:51 +0800

    初始化项目

---
+++ src/main/java/com/swpu/SwpucollegepelletizingApplication.java
@@ -0,0 +1,13 @@
+package com.swpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpucollegepelletizingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpucollegepelletizingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swpu/SwpucollegepelletizingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpucollegepelletizingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
