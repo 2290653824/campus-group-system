0a90c79cd861e35a4a6b3857547cf33569f8b320
Liyuxi <1318813286@qq.com>
2021-11-10 11:57:44 +0800

    上传mapper

---
+++ src/main/java/com/swpu/mapper/ProCategoryMapper.java
@@ -0,0 +1,16 @@
+package com.swpu.mapper;
+
+import com.swpu.entity.ProCategory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Liyuxi
+ * @since 2021-11-08
+ */
+public interface ProCategoryMapper extends BaseMapper<ProCategory> {
+
+}
